a930d4bf81e792c3b363335fb66edac130edcf1b
Aditya-1311-01 <adityakr6532@gmail.com>
2026-07-12 12:13:47 +0530

    Time: 35 ms (88.64%) | Memory: 39.6 MB (93.73%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+
+        int n = arr.size();
+
+        vector<int> ans(n);
+
+        priority_queue<pair<int,int>,
+                       vector<pair<int,int>>,
+                       greater<pair<int,int>>> pq;
+
+        for(int i = 0; i < n; i++){
+            pq.push({arr[i], i});
+        }
+
+        int rank = 0;
+        int prev = INT_MIN;
+
+        while(!pq.empty()){
+
+            auto [val, idx] = pq.top();
+            pq.pop();
+
+            if(rank == 0 || val != prev){
+                rank++;
+                prev = val;
+            }
+
+            ans[idx] = rank;
+        }
+
+        return ans;
+    }
+};
+        
+    
