ff8cc96d7f1942e80613281c2203dd07dced82f5
Aditya-1311-01 <adityakr6532@gmail.com>
2026-07-17 19:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (76.44%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        vector<int>freq(26,0);
+        if(s.size()>t.size() || s.size()<t.size()) return false;
+
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0)
+            return false;
+        }
+
+        return true;
+        
+    }
+};
