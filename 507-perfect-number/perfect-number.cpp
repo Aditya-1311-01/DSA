28d061d7c0613b2df6ad0dd6f4fbb06be6e63746
Aditya-1311-01 <adityakr6532@gmail.com>
2026-07-19 11:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (22.63%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int sum=0;
-        for(int i=1;i*i<num;i++){
+        for(int i=1;i*i<=num;i++){
             if(num%i==0){
                 sum+=i;
                 if(num/i!=i){
