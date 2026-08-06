49db858970dea0fb6f56d06ff5179dee5fd7a0e5
Aditya-1311-01 <adityakr6532@gmail.com>
2026-08-06 23:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,9 +13,16 @@ public:
     }
     int climbStairs(int n) {
 
+
         
         vector<int>dp(n+2,-1);
-        return count(0,n,dp);
+        dp[n]=1;
+        dp[n+1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+
+        return dp[0];
         
     }
 };
