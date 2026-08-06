aa3b9457cb7874d6790fb63d9111741fa5708ce8
Aditya-1311-01 <adityakr6532@gmail.com>
2026-08-06 21:47:37 +0530

    Time: 3 ms (64.82%) | Memory: 8.2 MB (11.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int fibonacci(int n,vector<int>&dp){
+    int fibon(int n,vector<int>&dp){
         if(n<=1)
         return n;
 
         if(dp[n]!=-1)
         return dp[n];
 
-        return dp[n]=fibonacci(n-1,dp)+fibonacci(n-2,dp);
+        return dp[n]=fibon(n-1,dp)+fibon(n-2,dp) ;// result store kr rhe
     }
+    
     int fib(int n) {
-        
         vector<int>dp(n+1,-1);
-         return fibonacci(n,dp);
+
+        return fibon(n,dp);
+        
+        
         
         
     }
