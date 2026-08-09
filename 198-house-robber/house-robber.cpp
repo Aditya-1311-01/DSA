1d1c9413ba25bb516866f3e4003e6bb993c3b52d
Aditya-1311-01 <adityakr6532@gmail.com>
2026-08-09 13:57:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (65.14%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -14,9 +14,15 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
+        if(n==1) return nums[0];
         vector<int>dp(n,-1);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
 
-        return robbery(n-1,nums,dp);
+        for(int i=2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
         
     }
 };
