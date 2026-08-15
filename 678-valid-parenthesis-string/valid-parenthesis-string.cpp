3d21ee6751be86740ee8acb4ebce38b01354c140
Aditya-1311-01 <adityakr6532@gmail.com>
2026-08-15 19:04:15 +0530

    Time: 15 ms (4.80%) | Memory: 25.9 MB (5.31%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int min=0,max=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                min=min+1;
-                max=max+1;
-            }
-            else if(s[i]==')'){
-                min=min-1;
-                max=max-1;
-            }
-            else{
-                min=min-1;
-                max=max+1;
-            }
-            if(min<0) min=0;
-            if(max<0) return false;
+    bool f(string s,int idx,int count,vector<vector<int>>&dp){
+        if(idx==s.size()){
+            return count==0;
         }
-        return min==0;
+        if(count<0) return false;
+
+        if(dp[idx][count]!=-1)
+        return dp[idx][count];
+
+        if(s[idx]=='(') return dp[idx][count]=f(s,idx+1,count+1,dp);
+        else if(s[idx]==')') return dp[idx][count]=f(s,idx+1,count-1,dp);
+
+        return dp[idx][count]=(f(s,idx+1,count-1,dp)|| f(s,idx+1,count,dp) || f(s,idx+1,count+1,dp));
+
+    }
+    bool checkValidString(string s) {
+        int n=s.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return f(s,0,0,dp);
     }
 };
