e590c3b779ce634de3130b31b677c307aede0384
Aditya-1311-01 <adityakr6532@gmail.com>
2026-08-20 23:08:18 +0530

    Time: 44 ms (18.88%) | Memory: 27.6 MB (32.21%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int f(int idx1,int idx2,string &s1,string &s2,vector<vector<int>>&dp){
-        if(idx1<0 || idx2<0) return 0;
+        if(idx1==0 || idx2==0) return 0;
 
         if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
 
-        if(s1[idx1]==s2[idx2])
+        if(s1[idx1-1]==s2[idx2-1])
         return dp[idx1][idx2]=1+f(idx1-1,idx2-1,s1,s2,dp);
 
         return dp[idx1][idx2]=max(f(idx1-1,idx2,s1,s2,dp), f(idx1,idx2-1,s1,s2,dp));
@@ -15,7 +15,7 @@ public:
         int m=text2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
-        return f(n-1,m-1,text1,text2,dp);
+        return f(n,m,text1,text2,dp);
         
     }
 };
