683831e8168717f430304df9deeb1ce35cc92bcb
Aditya-1311-01 <adityakr6532@gmail.com>
2026-08-21 14:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (73.95%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -17,7 +17,23 @@ public:
 
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
-        return operations(n-1,m-1,word1,word2,dp);
+        for(int i=0;i<=n;i++)
+        dp[i][0]=i;
+
+        for(int j=0;j<=m;j++)
+        dp[0][j]=j;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1])
+                dp[i][j]=0+dp[i-1][j-1];
+
+                else
+                dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+            }
+        }
+
+        return dp[n][m];
         
     }
 };
