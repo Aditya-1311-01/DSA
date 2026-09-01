66f785f513cc380bbd80ca9e1633d83178cb4b26
Aditya-1311-01 <adityakr6532@gmail.com>
2026-09-01 21:13:36 +0530

    Time: 2 ms (70.60%) | Memory: 18.2 MB (26.05%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,36 +1,67 @@
-class compare{
-    public:
-       bool operator()(pair<int,string>& a,pair<int,string>& b){
-            if(a.first == b.first){
-               return  a.second > b.second;
-            }
-            return a.first < b.first;
-       }
-};
-
 class Solution {
 public:
-    
+
+    struct compare {
+        bool operator()(const pair<int,string>& a,
+                        const pair<int,string>& b) {
+
+            if(a.first != b.first)
+                return a.first > b.first;
+
+            return a.second < b.second;
+        }
+    };
+
     vector<string> topKFrequent(vector<string>& words, int k) {
-        priority_queue<pair<int,string>, vector<pair<int,string>>, compare>p;
-        unordered_map<string,int>mpp;
 
-        for(int i =0;i<words.size();i++){
-            mpp[words[i]]++;
+        unordered_map<string,int> mpp;
+
+        // 1. Count frequency
+        for(string &word : words) {
+            mpp[word]++;
         }
 
-        for(auto it:mpp){
-            p.push(make_pair(it.second,it.first));
+        // 2. Min heap of size k
+        priority_queue<
+            pair<int,string>,
+            vector<pair<int,string>>,
+            compare
+        > p;
+
+        // 3. Process each unique word
+        for(auto &it : mpp) {
+
+            pair<int,string> curr = {
+                it.second,
+                it.first
+            };
+
+            if(p.size() < k) {
+                p.push(curr);
+            }
+            else {
+
+                // curr is better than the worst element
+                if(curr.first > p.top().first ||
+                   (curr.first == p.top().first &&
+                    curr.second < p.top().second)) {
+
+                    p.pop();
+                    p.push(curr);
+                }
+            }
         }
 
-        vector<string>ans;
-        pair<int,string> element;
-        while(k!=0){
-            element = p.top();
+        // 4. Extract
+        vector<string> ans;
+
+        while(!p.empty()) {
+            ans.push_back(p.top().second);
             p.pop();
-            ans.push_back(element.second);
-            k--;
         }
+
+        // Heap gives worst -> best
+        reverse(ans.begin(), ans.end());
 
         return ans;
     }
