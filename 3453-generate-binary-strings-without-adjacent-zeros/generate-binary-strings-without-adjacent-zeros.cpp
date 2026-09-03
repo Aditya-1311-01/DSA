81431f43c1ed04582bfb87be4dc1a73226186dc5
Aaditya <adityakr6532@gmail.com>
2026-09-03 12:38:12 +0530

    Time: 7 ms (90.61%) | Memory: 20.9 MB (80.24%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    void generate(int n, string curr, vector<string>& ans) {
-        if (curr.size() == n) {
-            ans.push_back(curr);
-            return;
-        }
-
-        // We can always add '1'
-        generate(n, curr + '1', ans);
-
-        // Add '0' only if previous character is not '0'
-        if (curr.empty() || curr.back() != '0') {
-            generate(n, curr + '0', ans);
-        }
+   void generate(int n, string& curr, vector<string>& ans) {
+    if (curr.size() == n) {
+        ans.push_back(curr);
+        return;
     }
 
+    // Choose 1
+    curr.push_back('1');
+    generate(n, curr, ans);
+    curr.pop_back();              // BACKTRACK
+
+    // Choose 0
+    if (curr.empty() || curr.back() != '0') {
+        curr.push_back('0');
+        generate(n, curr, ans);
+        curr.pop_back();          // BACKTRACK
+    }
+ } 
     vector<string> validStrings(int n) {
         vector<string> ans;
         string curr = "";
