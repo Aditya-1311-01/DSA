d8adcf269ab0abbdb6f059a5f78d7346b5cb91f1
Aaditya <adityakr6532@gmail.com>
2026-09-22 00:02:24 +0530

    Time: 171 ms (32.14%) | Memory: 181.4 MB (19.64%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
+
+    vector<vector<long long>> dp;
+    vector<int> nums;
+    int k;
+
+    void solve(int i) {
+
+        // Base case
+        if (i == 0) {
+            int rem = nums[0] % k;
+            dp[0][rem] = 1;
+            return;
+        }
+
+        // Pehle previous index calculate karo
+        solve(i - 1);
+
+        // Current element se new subarray
+        int rem = nums[i] % k;
+        dp[i][rem]++;
+
+        // Previous subarrays ko current element ke saath extend karo
+        for (int r = 0; r < k; r++) {
+
+            int newRem = (r * rem) % k;
+
+            dp[i][newRem] += dp[i - 1][r];
+        }
+    }
+
     vector<long long> resultArray(vector<int>& nums, int k) {
 
+        this->nums = nums;
+        this->k = k;
+
+        int n = nums.size();
+
+        dp.assign(n, vector<long long>(k, 0));
+
+        // Top-down recursion
+        solve(n - 1);
+
+        // Final answer
         vector<long long> ans(k, 0);
-        vector<long long> dp(k, 0);
 
-        for (int num : nums) {
-
-            vector<long long> newDp(k, 0);
-
-            // Start a new subarray with only nums[i]
-            newDp[num % k]++;
-
-            // Extend all previous subarrays
+        for (int i = 0; i < n; i++) {
             for (int r = 0; r < k; r++) {
-
-                if (dp[r] == 0)
-                    continue;
-
-                int newRem = (r * (num % k)) % k;
-
-                newDp[newRem] += dp[r];
+                ans[r] += dp[i][r];
             }
-
-            // All subarrays ending here contribute to answer
-            for (int r = 0; r < k; r++) {
-                ans[r] += newDp[r];
-            }
-
-            dp = newDp;
         }
 
         return ans;
