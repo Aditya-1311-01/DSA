4dff3e3a313078f2de19894dbcc6de6c17abe732
Aaditya <adityakr6532@gmail.com>
2026-09-26 00:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (49.01%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     void find(int idx,vector<int>& nums,vector<vector<int>>&ans,vector<int>&temp){
+        int n=nums.size();
+       
+       if(idx==n){
         ans.push_back(temp);
+        return;
+       }
 
-        for(int i=idx;i<nums.size();i++){
-            if(i!=idx && nums[i-1]==nums[i]) continue; // duplicate add nhi krega
+       temp.push_back(nums[idx]);
+       find(idx+1,nums,ans,temp);
+       temp.pop_back();
 
-            temp.push_back(nums[i]);
-            find(i+1,nums,ans,temp);
-            temp.pop_back();
-        }
+       while(idx<n-1 &&nums[idx]==nums[idx+1]){
+        idx++;
+       }
+       find(idx+1,nums,ans,temp);
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
